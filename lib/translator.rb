9dfb93b94a986f9c5eca8049fa7b9bbcd59a8c62
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-27 18:05:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -50,6 +50,7 @@ def get_japanese_emoticon(file, emoticon)
     lang_hash.each do |lang_key, lang_emoti|
       # binding.pry
       if emoticon == lang_hash[:english][lang_emoti]
+      end
         binding.pry
       # if emoticon == lang_hash[lang_key][lang_emoti]
       #   japanese_emoticon = lang_hash[lang_key][1]
