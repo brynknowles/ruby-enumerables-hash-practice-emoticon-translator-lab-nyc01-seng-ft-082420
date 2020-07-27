87774360c293727abdc047d9a0f323f8af050860
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-27 18:41:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -50,7 +50,7 @@ def get_japanese_emoticon(file, emoticon)
     if emoticon == value[:english]
       japanese_emoticon = value[:japanese]
     end
-    if emoticon != value
+    if emoticon != value[:english]
       apology
     end
     binding.pry
